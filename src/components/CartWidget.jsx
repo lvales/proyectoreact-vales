7f9e57fa2fd445a6b4679c6c9b83ea060f6724b1
Luciano Vales <lvales73@gmail.com>
2022-06-27 19:15:35 -0300

    Btn onAdd

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,18 +1,18 @@
 
+import { BsCart2 } from 'react-icons/bs';
+
 const CartWidget = ({ cantCart }) => {
-    return (
-        <div className="flex">
-            <div className="flex relative ml-5">
-                <button className="text-slate-900 font-semibold hover:text-slate-400" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                </button>
-                <div className="absolute -top-2 -right-2 bg-red-500 rounded-full px-1 font-medium text-xs text-slate-100"><p>{cantCart}</p></div>
-            </div>
-            <div className="ml-4">$ 0,00</div>
-        </div>
-    )
+	return (
+		<div className="flex">
+			<div className="flex relative ml-5">
+				<button className="text-slate-900 font-semibold hover:text-slate-400" href="#">
+					<BsCart2 className='text-2xl' />
+				</button>
+				<div className="absolute -top-2 -right-2 bg-red-500 rounded-full px-1 font-medium text-xs text-slate-100"><p>{cantCart}</p></div>
+			</div>
+			<div className="ml-4">$ 0,00</div>
+		</div>
+	)
 }
 
 export default CartWidget;
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,54 +1,57 @@
 import { useState } from "react";
 
-const ItemCount = ({ stock, initial}) => {
+const ItemCount = ({ stock, initial }) => {
 
-    const [amount, setAmount] = useState(parseInt(initial));
+	const [amount, setAmount] = useState(initial);
 
-    const add = () =>{
-        if (amount < parseInt(stock)) {
-            setAmount(amount + 1);
-        }
-    }
+	const add = () => {
+		if (amount < stock) {
+			setAmount(amount + 1);
+		}
+	}
 
-    const rest = () =>{
-        if (amount > 1) {
-            setAmount(amount - 1);
-        }
-    }
+	const rest = () => {
+		if (amount > 1) {
+			setAmount(amount - 1);
+		}
+	}
 
-    return (
-        <div className="bg-slate-100 w-1/6 rounded-lg">
-            <div className="p-5">
-                    <p className="text-lg font-medium">Bicicleta Olmo</p>
-                    <p className="text-sm">Stock: {stock}</p>
-                <div className="flex px-3 my-3 p-1 justify-between items-center border rounded-md bg-white">
-                    <div>
-                        <button className="text-2xl font-semibold text-sky-600"
-                            onClick={rest}>
-                                -
-                        </button>
-                    </div>
-                    <div>
-                        <p>{amount}</p>
-                    </div>
-                    <div>
-                        <button className="text-2xl font-semibold text-sky-600"
-                            onClick={add}>
-                                +
-                        </button>
-                    </div>
-                </div>
-                <div className="flex flex-row justify-between">
-                    <button className="py-2 px-4 bg-red-500 rounded-bl-xl text-slate-50 font-medium">
-                        Agregar al carrito
-                    </button>
-                    <button className="py-2 px-4 bg-slate-500 rounded-br-xl text-slate-50 font-medium">
-                        Ver carrito
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
+	const onAdd = () => alert('Producto agregado al carrito')
+
+	return (
+		<div className="bg-slate-100 w-1/6 rounded-lg">
+			<div className="p-5">
+				<p className="text-lg font-medium">Bicicleta Olmo</p>
+				<p className="text-sm">Stock: {stock}</p>
+				<div className="flex px-3 my-3 p-1 justify-between items-center border rounded-md bg-white">
+					<div>
+						<button className="text-2xl font-semibold text-sky-600"
+							onClick={rest}>
+							-
+						</button>
+					</div>
+					<div>
+						<p>{amount}</p>
+					</div>
+					<div>
+						<button className="text-2xl font-semibold text-sky-600"
+							onClick={add}>
+							+
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-row justify-between">
+					<button className="py-2 px-4 bg-red-500 rounded-bl-xl text-slate-50 font-medium"
+						onClick={onAdd}>
+						Agregar al carrito
+					</button>
+					<button className="py-2 px-4 bg-slate-500 rounded-br-xl text-slate-50 font-medium">
+						Ver carrito
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ItemCount;
